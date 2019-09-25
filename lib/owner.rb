ab2127f265717096d84d760e08f9c2eba97c36c7
Mike McTighe <m.mctighe10@gmail.com>
2019-09-25 17:41:44 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -56,7 +56,7 @@ end
   def sell_pets
     Cat.all.each{|cats| cats.mood = "nervous"}
     Dog.all.each{|dogs| dogs.mood = "nervous"}
-    Cat.all.each{|cats| cats.owner = "n"}
+    Cat.all.each{|cats| cats.owner = "}
     Dog.all.each{|dogs| dogs.owner  = "nervous"}
 
   end
