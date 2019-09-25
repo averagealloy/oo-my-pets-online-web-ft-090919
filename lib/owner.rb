1ae9d5418d18ee7841ce479405813e71cfe304de
Mike McTighe <m.mctighe10@gmail.com>
2019-09-25 14:32:08 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,12 +1,12 @@
 class Owner
   # code goes here
-attr_accessor :name, :species
+attr_re :name, :species
  @@all = []
 
   def initialize(name)
     @name = name
     @species = "human"
-    @@all << self 
+    @@all << self
   end
 
  def say_species
